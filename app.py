ffe66c4c340cf9c317d1820b462986b346f5ee75
aledgriffiths79 <aledgriffiths08@hotmail.co.uk>
2019-08-31 11:55:03 +0100

    adding render template

--- app.py
+++ app.py
@@ -21,10 +21,11 @@ mongo = PyMongo(app)
 @app.route('/')
 @app.route('/index')
 def index():
-  # """Homepage has 4 recipes from DB that have been viewed the most"""
+  """Homepage has 4 recipes from DB that have been viewed the most"""
   # four_recipes = mongo.db.recipes.find().sort([('views', DESCENDING)]).limit(4)
-  # return render_template('index.html', title='Home') #recipes=four_recipes)
-  return 'Hello'
+  return render_template('index.html', title='Home') #recipes=four_recipes)
+  # below is a setting stone to if my production site works
+  # return 'Hello'
 if __name__ == '__main__':
   # Local Host
   # app.run(host='127.0.0.1', debug=True)
